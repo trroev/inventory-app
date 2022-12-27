94c89c20989a4f4a67949be2eaa395c53981d5b3
Trevor Mathiak <tmathiak@gmail.com>
2022-12-27 13:46:22 -0500

    completed seafood form post request, and updated seafood_list.ejs

--- controllers/seafoodController.js
+++ controllers/seafoodController.js
@@ -1,11 +1,12 @@
 const Seafood = require("../models/seafood");
 
 const async = require("async");
+const { body, validationResult } = require("express-validator");
 
 exports.seafood_list = (req, res, next) => {
-  Seafood.find({}, "name price")
+  Seafood.find({}, "name price quantity")
     .sort({ name: 1 })
-    .populate("price")
+    .populate("price", "quantity")
     .exec(function (err, list_seafood) {
       if (err) {
         return next(err);
@@ -45,9 +46,59 @@ exports.seafood_create_get = (req, res) => {
   res.render("seafood_form", { title: "Create Seafood Item" });
 };
 
-exports.seafood_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Seafood create POST");
-};
+exports.seafood_create_post = [
+  body("name", "Seafood item name required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "a short description in required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "price is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("quantity", "quantity is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const seafood = new Seafood({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      quantity: req.body.quantity,
+    });
+    if (!errors.isEmpty()) {
+      res.render("seafood_form", {
+        title: "Create Seafood Item",
+        seafood,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      Seafood.findOne({ name: req.body.name }).exec(
+        (err, found_seafood) => {
+          if (err) {
+            return next(err);
+          }
+          if (found_seafood) {
+            res.redirect(found_seafood.url);
+          } else {
+            seafood.save((err) => {
+              if (err) {
+                return next(err);
+              }
+              res.redirect(seafood.url);
+            });
+          }
+        }
+      );
+    }
+  },
+];
 
 exports.seafood_delete_get = (req, res) => {
   res.send("NOT IMPLEMENTED: Seafood delete GET");
